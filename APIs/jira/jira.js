cd13a98ebc5f22cf72e48c7688fed430a19e9d0a
Piotr Karpala <pkarpala@infusion.com>
2016-12-05 00:23:08 -0500

    getting JIRA data

--- APIs/jira/jira.js
+++ APIs/jira/jira.js
@@ -1,6 +1,6 @@
 /*jshint esversion: 6, node: true*/
 'use strict';
-const Http = require('http');
+const Https = require('https');
 
 const harmonyKey = /HA-\d+/gi;
 
@@ -9,6 +9,53 @@ class jiraClient {
     static getJiraKeys(text) {
 
         return text.match(harmonyKey);
+    }
+
+    constructor(serverUrl, serverPort, username, password) {
+
+        this.serverUrl = serverUrl;
+        this.serverPort = serverPort;
+        this.auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+    }
+
+    createRequestOptions(method, key) {
+        //curl -u admin:admin http://localhost:8090/jira/rest/api/2/issue/MKY-1
+
+        return {
+            host: this.serverUrl,
+            port: this.serverPort,
+            path: `/rest/api/2/issue/${key}?fields=summary,created,status,aggregateprogress,priority,issuetype,customfield_11500,customfield_11700,customfield_10008`,
+            method: method || 'GET',
+            headers: {
+                Authorization: this.auth,
+                'Content-Type': 'application/json'
+            }
+        };
+    }
+
+    getJira(key, callback) {
+
+        const options = this.createRequestOptions('GET', key);
+
+        console.log(`Trying to get Jira from ${options.host}:${options.port}${options.path}`);
+
+        const req = Https.request(options, (res) => {
+
+            // res.statusCode
+            // res.headers
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+
+                callback(null, JSON.parse(chunk));
+            });
+        });
+
+        req.on('error', (e) => {
+
+            callback(e);
+        });
+
+        req.end();
     }
 }
 
--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,9 @@
 /*jshint esversion: 6, node: true*/
 'use strict';
 const Bitbucket = require('../../APIs/bitbucket/bitbucket');
+const JIRA = require('../../APIs/jira/jira');
 const bitbucketClient = new Bitbucket('localhost', 7990, 'PROJECT_1', 'rep_1', 'admin', 'admin');
+const jiraClient = new JIRA('ensemble.atlassian.net', 443, 'FORTracker', 'F0RTr@cker@Google');
 
 exports.register = function (server, options, next) {
 
@@ -55,6 +57,26 @@ exports.register = function (server, options, next) {
             });
         }
     });
+
+    server.route({
+        method: 'GET',
+        path: '/jira/{key}',
+        handler: function (request, reply) {
+
+            const key = request.params.key;
+
+            console.log('GET TEST - attempting to get JIRA: ' + key);
+            jiraClient.getJira(key, (err, res) => {
+
+                if (err) {
+                    return console.error(err);
+                }
+
+                reply(res);
+            });
+        }
+    });
+
 
     next();
 };
